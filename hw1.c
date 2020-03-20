6bd4d0f24c4d48e76d18dc71b048535499696451
bauuuu1021 <bboy010554@icloud.com>
2020-03-21 03:25:47 +0800

    Dump basic network connection information

--- hw1.c
+++ hw1.c
@@ -9,6 +9,13 @@
 #include <unistd.h>
 
 #define BUF_SIZE 1024
+#define skip_column(to_skip)                                                   \
+  do {                                                                         \
+    int column = to_skip;                                                      \
+    while (column-- > 0) {                                                     \
+      strtok(NULL, " ");                                                       \
+    }                                                                          \
+  } while (0)
 
 void parse_arg(int argc, char **argv) {
   int c;
@@ -66,7 +73,8 @@ int find_pid(int inode) {
   }
 
   while ((proc_content = readdir(proc_dir))) {
-    if ((!strcmp(proc_content->d_name, ".")) || (!strcmp(proc_content->d_name, "..")))
+    if ((!strcmp(proc_content->d_name, ".")) ||
+        (!strcmp(proc_content->d_name, "..")))
       continue;
 
     char path_to_fddir[BUF_SIZE];
@@ -83,17 +91,19 @@ int find_pid(int inode) {
           (!strcmp(fd_content->d_name, "..")))
         continue;
 
-      char symlink_dest[BUF_SIZE], path_to_symlink[BUF_SIZE], target_inode[BUF_SIZE];
+      char symlink_dest[BUF_SIZE], path_to_symlink[BUF_SIZE],
+          target_inode[BUF_SIZE];
       memset(symlink_dest, 0, sizeof(symlink_dest));
       memset(path_to_symlink, 0, sizeof(path_to_symlink));
       memset(target_inode, 0, sizeof(target_inode));
 
-      snprintf(path_to_symlink, sizeof(path_to_symlink), "%s/%s", path_to_fddir, fd_content->d_name);
+      snprintf(path_to_symlink, sizeof(path_to_symlink), "%s/%s", path_to_fddir,
+               fd_content->d_name);
       // printf("%s\n", path_to_symlink);
       readlink(path_to_symlink, symlink_dest, sizeof(symlink_dest));
       snprintf(target_inode, sizeof(target_inode), "socket:[%d]", inode);
       if (!strcmp(symlink_dest, target_inode)) {
-        printf("---%s---\n", path_to_symlink);
+        // printf("---%s---\n", path_to_symlink);
         closedir(fd_dir);
         closedir(proc_dir);
 
@@ -103,21 +113,39 @@ int find_pid(int inode) {
     closedir(fd_dir);
   }
   closedir(proc_dir);
-  return -1;  /* Search failed */
+  return -1; /* Search failed */
 }
 
 void tcp_conn() {
-  int i;
-  for (i=0; i<20; i++)
-  printf("pid %d\n", find_pid(25750));
-  /*
-  char buf[BUF_SIZE];
-  memset(buf, 0, sizeof(buf));
-  if(readlink("/proc/12324/fd/14", buf, sizeof(buf)) == -1) {
-    printf("%s\n", strerror(errno));
+
+  FILE *tcpv4 = fopen("/proc/net/tcp", "r");
+  char conn_info[BUF_SIZE], *local_addr, *foreign_addr, *inode;
+  memset(conn_info, 0, sizeof(conn_info));
+
+  /* Skip the title */
+  fgets(conn_info, BUF_SIZE, tcpv4);
+
+  while (fgets(conn_info, BUF_SIZE, tcpv4)) {
+    // printf("%s\n", conn_info);
+    strtok(conn_info, " ");
+    local_addr = strtok(NULL, " ");
+    foreign_addr = strtok(NULL, " ");
+    skip_column(6);
+    inode = strtok(NULL, " ");
+
+    int pid = find_pid(atoi(inode));
+    char cmd_path[BUF_SIZE];
+    memset(cmd_path, 0, sizeof(cmd_path));
+    snprintf(cmd_path, sizeof(cmd_path), "/proc/%d/cmdline", pid);
+    FILE *read_cmd = fopen(cmd_path, "r");
+    char cmd_content[BUF_SIZE];
+    memset(cmd_content, 0, sizeof(cmd_content));
+    fgets(cmd_content, BUF_SIZE, read_cmd);
+
+    printf("%s ; %s ; %d/%s\n", local_addr, foreign_addr, pid, cmd_content);
+    fclose(read_cmd);
   }
-  printf("%s\n", buf);
-  */
+  fclose(tcpv4);
 }
 
 int main(int argc, char **argv) {
