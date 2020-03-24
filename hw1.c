83c2bab77470858c2924abff95cda3da37f62fbe
bauuuu1021 <j851021@gmail.com>
2020-03-24 16:48:47 +0800

    Refine source code

--- hw1.c
+++ hw1.c
@@ -43,33 +43,26 @@ int find_pid(int inode) {
         (!strcmp(proc_content->d_name, "..")))
       continue;
 
-    char path_to_fddir[BUF_SIZE];
+    char path_to_fddir[BUF_SIZE] = {'\0'};
     snprintf(path_to_fddir, sizeof(path_to_fddir), "/proc/%s/fd",
              proc_content->d_name);
     if (!(fd_dir = opendir(path_to_fddir))) {
-      // printf("[find_pid] No Directory %s\n", path_to_fddir);
       continue;
     }
-    // printf("%s\n", proc_content->d_name);
 
     while ((fd_content = readdir(fd_dir))) {
       if ((!strcmp(fd_content->d_name, ".")) ||
           (!strcmp(fd_content->d_name, "..")))
         continue;
 
-      char symlink_dest[BUF_SIZE], path_to_symlink[BUF_SIZE],
-          target_inode[BUF_SIZE];
-      memset(symlink_dest, 0, sizeof(symlink_dest));
-      memset(path_to_symlink, 0, sizeof(path_to_symlink));
-      memset(target_inode, 0, sizeof(target_inode));
+      char symlink_dest[BUF_SIZE] = {'\0'}, path_to_symlink[BUF_SIZE] = {'\0'},
+           target_inode[BUF_SIZE] = {'\0'};
 
       snprintf(path_to_symlink, sizeof(path_to_symlink), "%s/%s", path_to_fddir,
                fd_content->d_name);
-      // printf("%s\n", path_to_symlink);
       readlink(path_to_symlink, symlink_dest, sizeof(symlink_dest));
       snprintf(target_inode, sizeof(target_inode), "socket:[%d]", inode);
       if (!strcmp(symlink_dest, target_inode)) {
-        // printf("---%s---\n", path_to_symlink);
         closedir(fd_dir);
         closedir(proc_dir);
 
@@ -92,8 +85,7 @@ int hex2int(char c) {
 }
 
 char *readable_format(char *origin) {
-  char ret[BUF_SIZE];
-  memset(ret, 0, sizeof(ret));
+  char ret[BUF_SIZE] = {'\0'};
   int split;
 
   if (!(origin[IPv4] - ':')) { /* IPv4 */
@@ -148,23 +140,18 @@ char *readable_format(char *origin) {
 
 void list_connection(char *protocal, char *filter) {
 
-  char network_file[BUF_SIZE];
-  memset(network_file, 0, sizeof(network_file));
+  char network_file[BUF_SIZE] = {'\0'};
   snprintf(network_file, sizeof(network_file), "/proc/net/%s", protocal);
   FILE *net_fp = fopen(network_file, "r");
-  char conn_info[BUF_SIZE], *local_addr, *foreign_addr, *inode;
-  memset(conn_info, 0, sizeof(conn_info));
+  char conn_info[BUF_SIZE] = {'\0'}, *local_addr, *foreign_addr, *inode;
 
   /* Skip the title */
   fgets(conn_info, BUF_SIZE, net_fp);
 
   while (fgets(conn_info, BUF_SIZE, net_fp)) {
     int pid;
-    char cmd_path[BUF_SIZE];
-    memset(cmd_path, 0, sizeof(cmd_path));
     FILE *read_cmd;
-    char cmd_content[BUF_SIZE];
-    memset(cmd_content, 0, sizeof(cmd_content));
+    char cmd_path[BUF_SIZE] = {'\0'}, cmd_content[BUF_SIZE] = {'\0'};
 
     strtok(conn_info, " ");
     local_addr = strtok(NULL, " ");
